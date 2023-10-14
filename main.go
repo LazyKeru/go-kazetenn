b000ab89b81c4258f1cb29d4dc0abf3c08caf0f4
LazyKeru <killian.allaire@hotmail.com>
2023-10-14 16:46:04 +0200

    can now change the logo img for the newsletter

--- main.go
+++ main.go
@@ -1,7 +1,47 @@
-package gokazetenn
+package main
 
-import "fmt"
+import (
+	"gokazetenn/articles/quote"
+	"gokazetenn/smtp"
+	"log"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+)
+
+func init() {
+
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	header := headerHTMLBuilder(os.Getenv("HEADER_LOGO"))
+	articles := []string{
+		quote.GetQuoteArticle(),
+		"<div>test2</div>",
+	}
+	smtp.SendNewsLetter(header, articles)
+}
+
+func headerHTMLBuilder(imgsrc string) string {
+	now := time.Now()
+	html := "<div style='display: flex; flex-direction: row;'>" +
+		"<div style='flex: 1; display: flex; align-items: center;justify-content: center;'>" +
+		"<img height='200em' src='" +
+		imgsrc +
+		"'>" +
+		"</div>" +
+		"<div style='flex: 2;'>" +
+		"<h1>Go Kazetenn</h1>" +
+		"<hr/>" +
+		"<h3>Your daily go's newsletter</h3>" +
+		"<h4>" + now.Format(time.RFC822) + "</h4>" +
+		"</div>" +
+		"</div>"
+	return html
 }
